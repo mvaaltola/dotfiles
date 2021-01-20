d1d45a64e4a58eba4d3be9699c5f880b763c6efe
Mikael Vaaltola <mvaaltola@gmail.com>
2021-01-20 13:26:54 +0200

    Add datacube env vars

--- .config/zsh/profile.d/datacube.sh
+++ .config/zsh/profile.d/datacube.sh
@@ -1,3 +1,5 @@
 export DATACUBE_DB_URL='postgresql://opendatacube:opendatacube@localhost:5432/opendatacube'
+export CFSI_BASE_DIR="$HOME/git/CFSI/cfsi"
+export CFSI_CONTAINER_OUTPUT="$HOME/files/cfsi_container_out"
 export CFSI_OUTPUT_DIR="$HOME/files/cfsi_out"
 
