e59f17028b6f9d4e0213fe3dbc723eb2f8f35dcc
Mikael Vaaltola <mvaaltola@gmail.com>
2021-02-26 10:21:40 +0200

    Update CFSI env vars

--- .config/zsh/profile.d/datacube.sh
+++ .config/zsh/profile.d/datacube.sh
@@ -1,5 +1,7 @@
 export DATACUBE_DB_URL='postgresql://opendatacube:opendatacube@localhost:5432/opendatacube'
-export CFSI_BASE_DIR="$HOME/git/CFSI/cfsi"
-export CFSI_CONTAINER_OUTPUT="$HOME/files/cfsi_container_out"
-export CFSI_OUTPUT_DIR="$HOME/files/cfsi_out"
-
+export CFSI_BASE_HOST="$HOME/git/CFSI/cfsi"
+# export CFSI_BASE_CONTAINER="$HOME/git/CFSI/cfsi"
+# export CFSI_CONFIG_FILE="config.yaml"
+export CFSI_OUTPUT_TYPE=LOCAL
+export CFSI_OUTPUT_HOST="$HOME/files/cfsi_out"
+export CFSI_OUTPUT_CONTAINER="$HOME/files/cfsi_container_out"
