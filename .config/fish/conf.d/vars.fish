94bdd3ca603e137400f1bd7c5e35e75576e8a073
Mikael Vaaltola <mvaaltola@gmail.com>
2021-09-22 11:11:18 +0300

    Add env vars, path dirs

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -1,5 +1,17 @@
 set -gx EDITOR nvim
-set -gx VISUAL nvim
+
+set -gx PAGER less
+set -gx LESS "S R"
+set -gx LESSOPEN "| /usr/bin/highlight -O ansi %s 2>/dev/null"
+set -gx LESSHISTFILE "-"
+set -gx BAT_THEME "TwoDark"
+
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+
+set -gx DOTBARE_DIR "$HOME/.config/dotfiles"
+set -gx WGETRC "$HOME/.config/wget/wgetrc"
 
 # For fish pj plugin
-set -gx PROJECT_PATHS ~/git ~/.config
+set -gx PROJECT_PATHS "$HOME/git" "$HOME/.config"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,12 @@
-
+# Add to path
+fish_add_path --path "$HOME/x"
+fish_add_path --path "$HOME/.local/bin"
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
     
     # Load wal terminal colors
-    cat ~/.cache/wal/sequences &
+    cat "$HOME/.cache/wal/sequences" &
 
     # Set vim keybinds
     fish_vi_key_bindings
