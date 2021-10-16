1d07dbf62d18b3b0ebe0607e6405a38068a3c9dd
Mikael Vaaltola <mvaaltola@gmail.com>
2021-10-16 19:30:44 +0300

    More git abbreviations

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -5,9 +5,12 @@ alias la="ls -alh"
 alias vim="nvim"
 alias vi="nvim"
 alias :q="exit"
+alias cfg="dotbare"
 
 abbr -a g git
 abbr -a gch git checkout
+abbr -a gbr git branch 
+abbr -a gre git remote
 abbr -a d docker
 abbr -a dc docker-compose
-alias cfg="dotbare"
+abbr -a pdeps pacman -Qdqt
