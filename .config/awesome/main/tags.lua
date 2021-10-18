d98df43fc7ca96ec02a739675dde3fa84f289bec
Mikael Vaaltola <mvaaltola@gmail.com>
2021-10-18 18:47:46 +0300

    Update tag/layout config

--- .config/awesome/main/tags.lua
+++ .config/awesome/main/tags.lua
@@ -7,12 +7,20 @@ local _M = {}
 
 function _M.get ()
   local tags = {}
+  tags = {
+    settings = {
+      { names  = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+        layout = RC.layouts[3]
+      },
+      { names  = { "1", "2", "3", "4", "5", "6", "7", "8", "9" },
+        layout = RC.layouts[1]
+      }
+    }
+  }
 
   awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
-    tags[s] = awful.tag(
-      { "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, RC.layouts[1]
-    )
+    tags[s] = awful.tag(tags.settings[s.index].names, s, tags.settings[s.index].layout)
   end)
   
   return tags
