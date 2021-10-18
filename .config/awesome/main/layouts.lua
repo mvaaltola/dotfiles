2c9654e6004ce6dc2be62074862e78efe8ae1640
Mikael Vaaltola <mvaaltola@gmail.com>
2021-10-18 23:53:27 +0300

    Use tile by default

--- .config/awesome/main/layouts.lua
+++ .config/awesome/main/layouts.lua
@@ -8,8 +8,8 @@ local _M = {}
 function _M.get ()
   -- Table of layouts to cover with awful.layout.inc, order matters.
   local layouts = {
-    awful.layout.suit.tile.left,          -- 1:
-    awful.layout.suit.tile,               -- 2:
+    awful.layout.suit.tile,               -- 1:
+    awful.layout.suit.tile.left,          -- 2:
     awful.layout.suit.tile.bottom,        -- 3:
     awful.layout.suit.tile.top,           -- 4:
 
