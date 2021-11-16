668d1bb7f3a6d0bd6f940a35cae43c6c227bbcd5
Mikael Vaaltola <mvaaltola@gmail.com>
2021-11-16 20:08:43 +0200

    Fix ssh abbr

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -7,7 +7,7 @@ alias vi="nvim"
 alias :q="exit"
 alias cfg="dotbare"
 
-abbr -a ssh = "kitty +kitten ssh"
+abbr -a ssh kitty +kitten ssh
 abbr -a g git
 abbr -a gch git checkout
 abbr -a gbr git branch 
