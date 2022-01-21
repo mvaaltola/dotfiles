9f6727155ced0d9ca932494c2aecb1cb6806fd1b
Mikael Vaaltola <mikael@vaalto.la>
2022-01-21 11:49:07 +0200

    Autoload virtual env

--- .config/fish/conf.d/pyenv.fish
+++ .config/fish/conf.d/pyenv.fish
@@ -5,4 +5,5 @@ set -gx PYENV_ROOT "$XDG_DATA_HOME/pyenv"
 function pyenvinit
   pyenv init --path | source
   pyenv init - | source
+  status --is-interactive; and pyenv-virtualenv-init - | source
 end
