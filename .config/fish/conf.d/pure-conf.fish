795a50f72c0f7be051e72f81ab04e47fb1174461
Mikael Vaaltola <mikael@vaalto.la>
2022-02-10 18:35:07 +0200

    Use pure_set_default function to set vars

--- .config/fish/conf.d/pure-conf.fish
+++ .config/fish/conf.d/pure-conf.fish
@@ -1,6 +1,8 @@
 # Custom config for pure
 
-set -u pure_show_system_time true
-set -u pure_color_primary blue
-set -u pure_symbol_prompt '$'
-set -u pure_symbol_reverse_prompt '\\'
+_pure_set_default pure_show_system_time true
+_pure_set_default pure_separate_prompt_on_error true
+_pure_set_default pure_show_prefix_root_prompt true
+_pure_set_default pure_color_primary blue
+_pure_set_default pure_symbol_prompt '$'
+_pure_set_default pure_symbol_reverse_prompt '\\'
