a77f6a99781a1ec4465db022836dfaa610f64c4f
Mikael Vaaltola <mikael@vaalto.la>
2022-09-21 16:31:50 +0300

    Add k8s config var

--- .config/fish/conf.d/vars.fish
+++ .config/fish/conf.d/vars.fish
@@ -14,5 +14,7 @@ set -gx XDG_CACHE_HOME "$HOME/.cache"
 set -gx DOTBARE_DIR "$HOME/.config/dotfiles"
 set -gx WGETRC "$HOME/.config/wget/wgetrc"
 
+set -gx KUBECONFIG "$XDG_CONFIG_HOME/kube/config"
+
 # For fish pj plugin
-set -gx PROJECT_PATHS "$HOME/app" "$HOME/.config" "$HOME/files"
+set -gx PROJECT_PATHS "$HOME/dev" "$HOME/.config" "$HOME/files"
