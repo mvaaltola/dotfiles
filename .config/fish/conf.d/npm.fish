0743fcf118ba416640f967edc86fcc1c48a3891a
Mikael Vaaltola <mikael.vaaltola@vaisala.com>
2022-10-05 12:36:14 +0300

    Skip setting npm manpath to avoid breaking search path

--- .config/fish/conf.d/npm.fish
+++ .config/fish/conf.d/npm.fish
@@ -1,4 +1,4 @@
 set -gx NPM_PACKAGES "$XDG_DATA_HOME/npm-packages"
 set -gx NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/npmrc"
 fish_add_path --path "$NPM_PACKAGES/bin"
-set MANPATH $NPM_PACKAGES/share/man $MANPATH
+#set MANPATH $NPM_PACKAGES/share/man $MANPATH
