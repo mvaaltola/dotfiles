25da4f2e82b26f24df88e82e82a1606d0a07494a
Mikael Vaaltola <mikael.vaaltola@vaisala.com>
2022-11-01 11:49:31 +0200

    Add basic nvim lua config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,13 @@
+-- helper function for mappings
+local function map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+
+vim.g.mapleader = ' '
+
+map('n', '<leader>R', ':so %<CR>')
