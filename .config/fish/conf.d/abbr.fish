132fb522adaada50d3fec5f5e2e7f6243125e1f0
Mikael Vaaltola <mikael.vaaltola@vaisala.com>
2023-10-30 14:53:50 +0200

    Add more git abbreviations

--- .config/fish/conf.d/abbr.fish
+++ .config/fish/conf.d/abbr.fish
@@ -16,18 +16,25 @@ abbr -a ssh kitty +kitten ssh
 
 abbr -a g git
 abbr -a ga git add
-abbr -a gch git checkout
+abbr -a gap git add -p
+abbr -a gch git switch
+abbr -a gr git restore
+abbr -a grs git restore --staged
 abbr -a gbr git branch 
 abbr -a gre git rebase
 abbr -a gd git diff
 abbr -a gds git diff --staged
 abbr -a gs git status --short --branch
+abbr -a gss git status
 abbr -a gst git stash
 abbr -a gsw git switch
 abbr -a gl git slog
+abbr -a glf git log --pretty=fuller
 abbr -a gll git log -p
 abbr -a gc git commit
 abbr -a gca git commit --amend --no-edit
+abbr -a gcar git commit --amend
+abbr -a gcan git commit --amend --no-edit --date=now
 abbr -a gm git merge --no-ff
 abbr -a gundo git reset --soft HEAD~1
 abbr -a gP git push --set-upstream
--- .config/fish/conf.d/pyenv.fish
+++
@@ -1,9 +0,0 @@
-# pyenv setup
-
-set -gx PYENV_ROOT "$XDG_DATA_HOME/pyenv"
-
-function pyenvinit
-  pyenv init --path | source
-  pyenv init - | source
-  status --is-interactive; and pyenv-virtualenv-init - | source
-end
