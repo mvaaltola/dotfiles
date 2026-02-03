d786656a8a9f74d1044459b510be0f79125ec971
Mikael Vaaltola <mikael.vaaltola@mapbox.com>
2026-02-03 13:30:28 +0200

    Update fish fzf keybinds

--- private_dot_config/fish/conf.d/fzf_custom.fish
+++ private_dot_config/fish/conf.d/fzf_custom.fish
@@ -1,5 +1,5 @@
 # Search directory with <c-f>
-fzf_configure_bindings --directory=\cf
+fzf_configure_bindings --directory=super-f --git_log=super-g --git_status=super-s
 
 # Show hidden files but ignore .git
 set -gx fzf_fd_opts --hidden --exclude=.git
@@ -10,5 +10,11 @@ set -gx fzf_preview_dir_cmd eza --all --color=always
 # Use delta for diff
 set -gx fzf_diff_highlighter delta --paging=never --width=20
 
-# Open file in editor with Ctrl+O
-set -gx fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+# Open file in nvim with Ctrl+O
+set -gx fzf_directory_opts --bind "ctrl-o:execute(nvim {} &> /dev/tty)"
+
+# Jump to project dirs
+function __fzfpj_bind --description 'Key binding for fzfpj'
+    fzfpj
+end
+bind -M insert super-p 'commandline -r "__fzfpj_bind"; commandline -f execute'
